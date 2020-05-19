28317c41a3d61e289ab5c2be043b37f3f9f0da99
unknown <irisjitomo@yahoo.com>
2020-05-19 15:21:29 -0400

    made Login2 with formik and cookies

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
+import Login2 from './components/Login2'
 import "./styles.scss";
 import BubblePage from "./components/BubblePage";
 import PrivateRoute from './components/PrivateRoute';
@@ -13,7 +14,8 @@ function App() {
       <div className="App">
         <Switch>
           <PrivateRoute exact path="/protected" component={BubblePage} />
-        <Route exact path="/" component={Login} />
+        {/* <Route exact path="/" component={Login} /> */}
+        <Route exact path="/" component={Login2}/>
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
